c5cda51e4cade223566ed18145fd034ef5ce6e4f
Marcel Prapas Krissanta <80663507+marcelprapassss@users.noreply.github.com>
2025-10-13 17:20:27 +0700

    Create tugas_praktikum_1.php

---
+++ praktikum_1/tugas_praktikum_1.php
@@ -0,0 +1,15 @@
+<?php
+$person_name = array("Marcel", "Ardhan", "Ayu", "Albert");
+
+echo json_encode($person_name);
+
+$name_age = '{"Marcel":22, "Ardhan":22, "Ayu":18, "Albert":23}';
+echo '<br>';
+$obj = json_decode($name_age);
+
+//mengakses nilai object
+echo $obj->Marcel;
+echo $obj->Ardhan;
+echo $obj->Ayu;
+echo $obj->Albert;
+?>
